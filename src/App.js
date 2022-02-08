1ac0a6992488b8c54fc957d91baea04f15e7b1ef
nlister-makor <neriatest@gmail.com>
2022-02-08 17:55:52 +0200

    merged with url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Grid } from '@material-ui/core';
 import './App.css';
 import Password from './Components/Password';
-
+import UrlValidation from './Components/UrlValidation';
 function App() {
   return (
     <div>
-      <Grid container style={{ display: 'flex' }}>
+      <Grid container style={{ display: 'flex' }} direction="column">
         <Password />
+        <UrlValidation />
       </Grid>
     </div>
   );
---
+++ src/Components/UrlValidation.js
@@ -0,0 +1,55 @@
+import { Button, Grid, TextField } from '@material-ui/core'
+
+import { URLValidator } from 'enigma-x-utilities'
+
+import { useEffect, useState } from 'react'
+
+function UrlValidation() {
+  const [URL, setURL] = useState('')
+  const [processedURL, setProcessedURL] = useState(undefined)
+  const [message, setMessage] = useState('')
+  const [success, setSuccess] = useState(undefined)
+  const CheckURL = () => {
+    const URLResponse = URLValidator(URL)
+    if (URLResponse.success) {
+      setMessage(URLResponse.message)
+      setProcessedURL(URLResponse.data)
+      setSuccess(URLResponse.success)
+    } else {
+      setMessage(URLResponse.message)
+      setSuccess(URLResponse.success)
+    }
+  }
+  useEffect(() => {}, [URL])
+
+  return (
+    <Grid item xs={5} style={{ borderRadius: 8, borderRadius: 8, border: '1px solid black' }}>
+      <Grid container alignItems="center" justifyContent="space-evenly">
+        <Grid item xs={11} style={{ marginBottom: '5%' }}>
+          {success == true ? <div>dfs</div> : <div></div>}
+          {processedURL && <h3> URL: {processedURL}</h3>}
+        </Grid>
+        <Grid item xs={5}>
+          <TextField
+            id="outlined-basic"
+            label="Enter URL"
+            variant="outlined"
+            style={{ width: '150%' }}
+            onChange={(e) => {
+              setURL(e.target.value);
+            }}
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <Button style={{ marginLeft: '50%' }} size="large" onClick={(e) => CheckURL(e)}>
+            Check URL
+          </Button>
+          <Grid container></Grid>
+        </Grid>
+        <Grid item xs={11} style={{ paddingTop: 30 }}></Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default UrlValidation;
