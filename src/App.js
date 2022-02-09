c492bb621b66df96a530bb3dc867361bb4f6833c
unknown <ochayoun@makor-capital.com>
2022-02-09 08:55:43 +0200

    phone validator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { Grid } from '@material-ui/core';
-import './App.css';
-import Password from './Components/Password';
-import UrlValidation from './Components/UrlValidation';
+import { Grid } from '@material-ui/core'
+import './App.css'
+import Password from './Components/Password'
+import UrlValidation from './Components/UrlValidation'
+import PhoneValidation from './Components/PhoneValidation'
 function App() {
   return (
     <div>
-      <Grid container style={{ display: 'flex' }} direction="column">
+      <Grid container style={{ display: 'flex' }} direction='column'>
         <Password />
         <UrlValidation />
+        <PhoneValidation />
       </Grid>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
